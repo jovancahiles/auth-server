3b1d1f76f32e6369a831166732fff1cc1db30059
Ariel M <ariel.macapas@gmail.com>
2018-09-25 20:48:55 +0800

    auth

--- app.js
+++ app.js
@@ -1,9 +1,58 @@
+/**
+ * Stripe Authorization
+ * Handle authorization requests made during a card purchase.
+ * https://stripe.com/docs/api#list_issuing_authorizations
+ * https://stripe.com/docs/issuing/authorizations#authorization-handling
+ *
+ */
+
 const express = require('express')
 const app = express()
 const port = 9000
 
-app.get('/', (request, response) => {
-  response.send('Brinq - Employee development made easy.')
+const stripe = require('stripe')(process.env.STRIPE_TOKEN)
+
+app.get('/', (req, res) => {
+  res.send('Brinq - Employee development made easy.')
+})
+
+app.post('/authorize', (req, res) => {
+  console.log(req);
+  const { authorizationID } = req.body;
+
+  await stripe.issuing.authorizations.approve({ authorizationID },
+    (err, authorization) => {
+      if (err) throw new Error(err);
+      res.send(authorization);
+    });
+
+  // deny
+  // await stripe.issuing.authorizations.decline({ authorizationID },
+  //   (err, authorization) => {
+  //     if (err) throw new Error(err);
+  //     res.send(authorization);
+  //   });
+  res.send('Process auth request.')
+})
+
+app.get('/list', (req, res) => {
+  // const { limit } = req.body;
+
+  // await stripe.issuing.authorizations.list({ limit },
+  //   (err, authorization) => {
+  //     if (err) throw new Error(err);
+  //     res.send(authorization);
+  //   });
+})
+
+app.get('/get', (req, res) => {
+  // const { authorizationID } = req.body;
+
+  // await stripe.issuing.authorizations.retrieve({ authorizationID },
+  //   (err, authorization) => {
+  //     if (err) throw new Error(err);
+  //     res.send(authorization);
+  //   });
 })
 
 app.listen(port, (err) => {
