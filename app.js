bd6e408542f8db35f795c1437361187255a88e51
James Delorey <deloreyjmi@gmail.com>
2019-01-16 22:08:12 -0700

    update app

--- app.js
+++ app.js
@@ -9,25 +9,21 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 
-const bodyParser = require('body-parser')
 const stripe = require('stripe')(process.env.STRIPE_TOKEN)
-
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   res.send('Brinq - Employee development made easy.')
 })
 
-app.post('/authorize', (req, res) => {
-  const { type, data } = req.body
-  const stripeSig = req.headers['stripe-signature']
-  console.log('request body', data);
+// app.post('/authorize', (req, res) => {
+//   const { type, data } = req.body
+//   const stripeSig = req.headers['stripe-signature']
+//   console.log('request body', data);
 
-  console.log(stripeSig)
-})
+//   console.log(stripeSig)
+// })
 
-app.listen(3000, (err) => {
+app.listen(80, (err) => {
   if (err) {
     return console.log('something bad happened', err)
   }
