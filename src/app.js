609e005dc50f756ac75c142c29813663e4f8f102
jcahiles <jovancahiles@gmail.com>
2019-08-15 18:45:58 +0800

    wrapped updated buget with number constructor

--- src/app.js
+++ src/app.js
@@ -4,16 +4,16 @@
  * https://stripe.com/docs/issuing/authorizations#authorization-handling
  *
  */
-require("dotenv").config();
+require("dotenv").config()
 
-const admin = require("firebase-admin");
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const axios = require("axios");
+const admin = require("firebase-admin")
+const express = require("express")
+const app = express()
+const bodyParser = require("body-parser")
+const cors = require("cors")
+const axios = require("axios")
 
-console.log(typeof process.env.FIREBASE_KEY);
+console.log(typeof process.env.FIREBASE_KEY)
 
 admin.initializeApp({
   credential: admin.credential.cert({
@@ -25,41 +25,41 @@ admin.initializeApp({
     )
   }),
   databaseURL: "https://brinq-reimbursements.firebaseio.com"
-});
+})
 
-const db = admin.firestore();
-let running = false;
+const db = admin.firestore()
+let running = false
 
-const budgets = {};
+const budgets = {}
 db.collection("budgets").onSnapshot(snapshot => {
   snapshot.forEach(doc => {
-    budgets[doc.id] = doc.data().budget;
-  });
+    budgets[doc.id] = doc.data().budget
+  })
   if (!running) {
-    app.emit("ready");
+    app.emit("ready")
   }
-});
+})
 
-const stripe = require("stripe")(process.env.STRIPE_TOKEN);
+const stripe = require("stripe")(process.env.STRIPE_TOKEN)
 
 const networkIds = [
   "000174101933995",
   "000445136930994",
   "000227470792994",
   "000445191569992"
-];
+]
 
-app.use(bodyParser.json()); // parsing application/json
-app.use(cors());
+app.use(bodyParser.json()) // parsing application/json
+app.use(cors())
 
 app.get("/", (req, res) => {
-  res.send("Brinq - Employee development made easy.");
-  console.log("budgets", budgets);
-});
+  res.send("Brinq - Employee development made easy.")
+  console.log("budgets", budgets)
+})
 
 app.post("/authorize", async (req, res) => {
   try {
-    const start = Date.now();
+    const start = Date.now()
     let {
       id,
       authorization_method,
@@ -67,35 +67,35 @@ app.post("/authorize", async (req, res) => {
       pending_authorized_amount,
       cardholder,
       merchant_data: { network_id }
-    } = req.body.data.object;
+    } = req.body.data.object
 
     if (authorization_method === "online" && authorized_currency === "usd") {
       if (!budgets[cardholder]) {
-        stripe.issuing.authorizations.decline(id);
-        res.status(400).send("No such user");
+        stripe.issuing.authorizations.decline(id)
+        res.status(400).send("No such user")
       }
-      const budget = budgets[cardholder] * 100;
+      const budget = budgets[cardholder] * 100
       if (budget > pending_authorized_amount) {
-        const newBudget = (budget - pending_authorized_amount) / 100;
-        console.log("authorizing", Date.now() - start);
+        const newBudget = (budget - pending_authorized_amount) / 100
+        console.log("authorizing", Date.now() - start)
         return await stripe.issuing.authorizations
           .approve(id)
           .then(async stripeRes => {
             console.log(
               "responding to authorization succeeded",
               Date.now() - start
-            );
+            )
             axios.post(
               "https://hooks.slack.com/services/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",
               {
                 text: `Auth server approved authorization of ${pending_authorized_amount} for ${cardholder}. Had ${budget} and now has ${newBudget}`
               }
-            );
+            )
             await db
               .collection("budgets")
               .doc(cardholder)
-              .update({ budget: newBudget.toFixed(2) });
-            res.status(200).send("Authorization approved");
+              .update({ budget: Number(newBudget.toFixed(2)) })
+            res.status(200).send("Authorization approved")
           })
           .catch(e => {
             axios.post(
@@ -103,29 +103,26 @@ app.post("/authorize", async (req, res) => {
               {
                 text: `Responding to authorization request threw an error from Stripe: ${e} for ${cardholder}`
               }
-            );
-            console.log(
-              "responding to authorization threw",
-              Date.now() - start
-            );
-            console.log("approving the authorization threw", e);
-            res.status(500).send("Authorization failed");
-          });
+            )
+            console.log("responding to authorization threw", Date.now() - start)
+            console.log("approving the authorization threw", e)
+            res.status(500).send("Authorization failed")
+          })
       } else {
-        console.log("would not authorize");
+        console.log("would not authorize")
         axios.post(
           "https://hooks.slack.com/services/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",
           {
             text: `Auth server rejected authorization because of insufficient funds. Requested ${pending_authorized_amount} and had ${budget} available budget for ${cardholder}`
           }
-        );
+        )
         return await stripe.issuing.authorizations
           .decline(id)
           .finally(result => {
             res
               .status(400)
-              .send("Authorization rejected because of insufficient funds");
-          });
+              .send("Authorization rejected because of insufficient funds")
+          })
       }
     } else {
       axios.post(
@@ -133,22 +130,22 @@ app.post("/authorize", async (req, res) => {
         {
           text: `Auth server rejected authorization because of incorrect authorization_method (${authorization_method}) or authorization_currency (${authorized_currency}) for ${cardholder}`
         }
-      );
-      console.log("would not authorize");
-      stripe.issuing.authorizations.decline(id);
-      res.status(400).send("Authorization rejected");
+      )
+      console.log("would not authorize")
+      stripe.issuing.authorizations.decline(id)
+      res.status(400).send("Authorization rejected")
     }
   } finally {
-    console.log(e);
+    console.log(e)
     axios.post(
       "https://hooks.slack.com/services/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",
       {
         text: `Auth Server caught an error with the input`
       }
-    );
-    res.status(400).send(`Malformed input`);
+    )
+    res.status(400).send(`Malformed input`)
   }
-});
+})
 
 app.on("ready", () => {
   app.listen(8080, err => {
@@ -158,16 +155,16 @@ app.on("ready", () => {
         {
           text: `Auth server pod errored on restart: ${err}`
         }
-      );
-      return console.log("something bad happened", err);
+      )
+      return console.log("something bad happened", err)
     }
-    running = true;
-    console.log(`server is listening on 8080`);
+    running = true
+    console.log(`server is listening on 8080`)
     axios.post(
       "https://hooks.slack.com/services/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",
       {
         text: "Auth server pod restarted successfully"
       }
-    );
-  });
-});
+    )
+  })
+})
